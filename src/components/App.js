9f913226c993f05649aa3772a859044d66db2744
DESKTOP-HKJ5PIB\adity <aditya.shah7023@gmail.com>
2018-12-08 21:32:05 -0500

    Added UI template

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,21 @@ import Header from "./Header";
 import ForecastTable from "./ForecastTable";
 
 class App extends React.Component {
+  state = {
+    location: null
+  }
+
+  getWeather = (city) => {
+    //Weather.getCurrent(city, function(current){ 
+      console.log(city);
+    //});
+  }
+
   render(){
     return (
       <React.Fragment>
         <Header appName="The Weather App" tagLine="By Shah, Aditya"/>
-        <RetrieveLocation />
+        <RetrieveLocation getWeather={this.getWeather}/>
         <ForecastTable />
       </React.Fragment>
     );
--- src/components/RetrieveLocation.js
+++ src/components/RetrieveLocation.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 class RetrieveLocation extends React.Component {
+	locationRef = React.createRef();
+
+	addLocation = event => {
+		event.preventDefault();
+		this.props.getWeather(this.locationRef);
+		event.currentTarget.reset();
+	};
+
   render() {
     const Background = "/images/banner.png";
     return(
       <div className="hero" style={{backgroundImage: `url(${Background})`}}>
 				<div className="container">
-					<form action="#" className="find-location">
-						<input type="text" placeholder="Find your location..."/>
+					<form className="find-location" onSubmit={this.addLocation}>
+						<input type="text" placeholder="Find your location..." ref={this.locationRef}/>
 						<input type="submit" value="Find"/>
 					</form>
 
